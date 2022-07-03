bd94150312614a7eb4c4068590395bff48503123
KajitaYuto <k021g1149@g.neec.ac.jp>
2022-07-03 15:51:15 +0900

    09

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -13,6 +13,26 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //’e‚Ìƒ[ƒ‹ƒhÀ•W‚ðŽæ“¾
+        Vector3 pos = transform.position;
+
+        //ã‚É‚Ü‚Á‚·‚®”ò‚Ô
+        pos.z += 0.05f;
+
+        //’e‚ÌˆÚ“®
+        transform.position = new Vector3(pos.x, pos.y, pos.z);
+
+        //ˆê’è‹——£i‚ñ‚¾‚çÁ–Å‚·‚é
+        if (pos.z >= 20){
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.GetComponent<Enemy>().Damage();
+        }
     }
 }
--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -14,16 +14,19 @@ public class PlayerMove : MonoBehaviour
     void Update(){
         Vector3 pos = transform.position;
 
-        if (Input.GetKey(KeyCode.RightArrow)){
+        if (Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D)){
             pos.x += 0.01f;
         }
-        if (Input.GetKey(KeyCode.LeftArrow)){
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
             pos.x -= 0.01f;
         }
-        if (Input.GetKey(KeyCode.UpArrow)){
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
             pos.z += 0.01f;
         }
-        if (Input.GetKey(KeyCode.DownArrow)){
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
             pos.z -= 0.01f;
         }
         transform.position = new Vector3(pos.x, pos.y, pos.z);
